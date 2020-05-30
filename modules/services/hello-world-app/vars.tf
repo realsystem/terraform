cb91b8fe639f71961252a6bf70174cfdb9c164f9
Stanislav Egorov <segorov@mirantis.com>
2020-05-29 23:18:32 -0700

    added lost vars

--- modules/services/hello-world-app/vars.tf
+++ modules/services/hello-world-app/vars.tf
@@ -12,3 +12,54 @@ variable "environment" {
 	description = "The name of the environment we're deploying to"
 	type = string
 }
+
+variable "server_text" {
+	description = "The text the webserver should return"
+	type = string
+	default = "Hello, world"
+}
+
+variable "custom_tags" {
+	description = "Custom tags to set on the Instances in the ASG"
+	type = map(string)
+	default = {}
+}
+
+variable "enable_autoscaling" {
+	description = "If set to true enable autoscaling"
+	type = bool
+}
+
+variable "ami" {
+	description = "The AMI to run in the cluster"
+	type = string
+	default = "ami-0e84e211558a022c0"
+}
+
+variable "cluster_name" {
+	description = "The name to use for all the cluster resources"
+	type = string
+}
+
+variable "min_size" {
+	description = "The minimum number of instances"
+	type = number
+	default = 1
+}
+
+variable "desired_capacity" {
+	description = "The desired number of instances"
+	type = number
+	default = 1
+}
+
+variable "max_size" {
+	description = "The maximum number of instances"
+	type = number
+	default = 5
+}
+
+variable "instance_type" {
+	description = "The type of the instance"
+	type = string
+}
--- stage/services/hello-world-app/main.tf
+++ stage/services/hello-world-app/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "hello_world_app" {
-	source = "git@github.com:realsystem/terraform.git//modules/services/hello-world-app?ref=v0.0.1"
+	source = "git@github.com:realsystem/terraform.git//modules/services/hello-world-app?ref=v0.0.2"
 	cluster_name = "webservers-stage"
 	environment = "stage"
 	instance_type = "t2.micro"
